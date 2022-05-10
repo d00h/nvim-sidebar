8fa1b2ed2c01ee6ad8c0f289db1e8537e2d51558
Valerii Volkov <v.volkov@start.ru>
2022-05-10 13:12:55 +0300

    git-show

--- lua/nvim-sidebar/impl/git_log.lua
+++ lua/nvim-sidebar/impl/git_log.lua
@@ -5,56 +5,75 @@ local get_current_line = require('nvim-sidebar.buffer').get_current_line
 
 local show_window = require('nvim-sidebar.window').show
 
-local Job = require('plenary.job')
+local Job = require 'plenary.job'
+local NAMESPACE = 'nvim-sidebar.impl.git_log'
 
 ----
 
 local M = {}
 
 M.setup_keys = function(bufnr)
-    local opts = {noremap = true, silent = true}
-    local nvim_buf_set_keymap = vim.api.nvim_buf_set_keymap
+  local opts = { noremap = true, silent = true }
+  local nvim_buf_set_keymap = vim.api.nvim_buf_set_keymap
 
-    -- nvim_buf_set_keymap(bufnr, 'n', '<cr>',
-    --                     "<cmd>lua require('nvim-sidebar.impl.git_status').open_child()<cr>",
-    --                     opts)
-    -- nvim_buf_set_keymap(bufnr, 'n', 'l',
-    --                     "<cmd>lua require('nvim-sidebar.impl.git_status').open_child()<cr>",
-    --                     opts)
-    nvim_buf_set_keymap(bufnr, 'n', 'h', "<cmd>Sidebar menu<cr>", opts)
-    nvim_buf_set_keymap(bufnr, 'n', 'q', "<cmd>bdelete<cr>", opts)
+  nvim_buf_set_keymap(bufnr, 'n', '<cr>', "<cmd>lua require('" .. NAMESPACE .. "').open_child()<cr>", opts)
+  nvim_buf_set_keymap(bufnr, 'n', 'l', "<cmd>lua require('" .. NAMESPACE .. "').open_child()<cr>", opts)
+  nvim_buf_set_keymap(bufnr, 'n', 'h', '<cmd>Sidebar menu<cr>', opts)
+  nvim_buf_set_keymap(bufnr, 'n', 'q', '<cmd>bdelete<cr>', opts)
 end
 
 M.open = function(args)
-    delete_all_buffers()
+  delete_all_buffers()
 
-    local bufnr = create_buffer('')
-    local win = show_window(bufnr)
+  local bufnr = create_buffer ''
+  local win = show_window(bufnr)
 
-    update_buffer(bufnr, {'...waiting...'})
+  update_buffer(bufnr, { '...waiting...' })
 
-    local on_exit = function(job, errorlevel)
-        vim.schedule(function()
-            update_buffer(bufnr, job:result())
-            M.setup_keys(bufnr)
-        end)
-    end
+  local on_exit = function(job, errorlevel)
+    vim.schedule(function()
+      update_buffer(bufnr, job:result())
+      M.setup_keys(bufnr)
+    end)
+  end
 
-    Job:new({
-        command = 'git',
-        args = {'log', '--oneline', unpack(args)},
-        on_exit = on_exit
-    }):start()
+  Job
+    :new({
+      command = 'git',
+      args = { 'log', '--oneline', unpack(args) },
+      on_exit = on_exit,
+    })
+    :start()
 end
 
 M.open_child = function()
-    -- local current_line =get_current_line(0, 0)
-    -- local filename = string.gsub(current_line, '^%s*%w+%s+', '')
-    -- local selected = Path:new(filename)
-    --
-    -- if selected:is_file() then
-    --     vim.cmd('wincmd l | edit ' .. tostring(selected))
-    -- end
+  local current_line = get_current_line(0, 0)
+  local commit = string.match(current_line, '^%S+')
+
+  vim.cmd 'wincmd l'
+  local bufnr = vim.api.nvim_create_buf(true, true)
+  --
+  vim.api.nvim_buf_set_option(bufnr, 'buftype', 'nofile')
+  vim.api.nvim_win_set_buf(0, bufnr)
+
+  update_buffer(bufnr, { '...waiting...' })
+
+  local on_exit = function(job, errorlevel)
+    vim.schedule(function()
+      update_buffer(bufnr, job:result())
+      vim.api.nvim_buf_set_option(bufnr, 'filetype', 'diff')
+      local last_line = vim.api.nvim_buf_line_count(bufnr)
+      vim.api.nvim_win_set_cursor(0, { last_line, 0 })
+    end)
+  end
+
+  Job
+    :new({
+      command = 'git',
+      args = { 'show', commit },
+      on_exit = on_exit,
+    })
+    :start()
 end
 
 return M
